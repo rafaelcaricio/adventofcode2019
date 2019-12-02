fab8421401deff8a4bb6003252d58744a2773983
Rafael Caricio <rafael.caricio@fireeye.com>
2019-12-01 21:30:08 -0300

    Day 1 - Part 2

--- advent01/src/main.rs
+++ advent01/src/main.rs
@@ -14,9 +14,37 @@ fn main() -> io::Result<()> {
             }
         };
         buf.clear();
-        let part = (mass / 3f64).floor() as u64;
-        acc += part - 2;
+        acc += calculate_fuel(mass) as u64;
     }
     println!("Result: {}", acc);
     Ok(())
+}
+
+fn calculate_fuel(mass: f64) -> f64 {
+    let required_fuel = (mass / 3f64).floor() - 2f64;
+    if required_fuel > 0f64 {
+        required_fuel + calculate_fuel(required_fuel)
+    } else {
+        0f64
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example1() {
+        assert_eq!(calculate_fuel(14f64), 2f64);
+    }
+
+    #[test]
+    fn test_example2() {
+        assert_eq!(calculate_fuel(1969f64), 966f64);
+    }
+
+    #[test]
+    fn test_example3() {
+        assert_eq!(calculate_fuel(100756f64), 50346f64);
+    }
 }
